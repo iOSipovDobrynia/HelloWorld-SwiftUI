95aa4d850dfea7bdd6e7de91434b2c07a7867afc
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-04 19:30:45 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Goodwasp on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
